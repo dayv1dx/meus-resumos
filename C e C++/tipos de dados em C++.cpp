f29d0724f6a96579085da9e91f3c9c08c2de641b
Dayvid Cunha <dayv1dxdev@gmail.com>
2023-05-10 11:46:11 -0300

    adiciona os tipos de dados em C e C++

---
+++ C e C++/tipos de dados em C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    
+    int numero = 10;
+    float pi = 3.1415;
+    double numero_maior = 99999999999999999999999999.999;
+    char letra = 'a';
+    string nome = "Dayv1dx";
+
+    return 0;
+}
---
+++ C e C++/tipos de dados em C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int numero = 10;
+    float pi = 3.1415;
+    double numero_maior = 99999999999999999999999999.999;
+    char letra = 'a';
+    // String 
+    char nome[10] = "Dayv1dx";
+    
+    printf("O valor do nome é %s\n", nome);
+
+    return 0;
+}
