2551ba3b54fc35b3737dcf4e69a709c705f971d5
Dayvid Cunha <dayv1dxdev@gmail.com>
2023-05-10 11:16:50 -0300

    adiciona os primeiros conteúdos de C e C++

---
+++ C e C++/estrutura básica de um programa em C e C++.cpp
@@ -0,0 +1,17 @@
+// ESTRUTURA BÁSICA DE UM PROGRAMA EM C++
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"olá, mundo em C++" << endl;
+    return 0;
+}
+
+// ESTRUTURA BÁSICA DE UM PROGRAMA EM C
+#include<stdio.h>
+
+int main( void ){
+    puts("olá, mundo em C");
+    return 0;
+}
